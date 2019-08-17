92b14ac906386f2a244af1676455c1bb670f94d0
Alain Tran <alaint77@gmail.com>
2019-08-17 14:27:35 +0200

    update learning

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,11 @@ function style() {
   // 1 where is my scss file
   return (
     gulp
-      .src("./scss/**/*.scss")
+      .src("./src/scss/**/*.scss")
       // 2 pass that file through sass compiler
       .pipe(sass().on("error", sass.logError))
       // 3 where do i save the compiled css?
-      .pipe(gulp.dest("./css"))
+      .pipe(gulp.dest("./src/css"))
       // 4 stream changes to all browser
       .pipe(browserSync.stream())
   );
@@ -20,12 +20,12 @@ function style() {
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "./"
+      baseDir: "./src"
     }
   });
-  gulp.watch("./scss/**/*.scss", style);
-  gulp.watch("./*.html").on("change", browserSync.reload);
-  gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+  gulp.watch("./src/scss/**/*.scss", style);
+  gulp.watch("./src/*.html").on("change", browserSync.reload);
+  gulp.watch("./src/js/**/*.js").on("change", browserSync.reload);
 }
 
 exports.style = style;
